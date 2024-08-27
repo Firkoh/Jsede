2420b34bcf1dc3f52007affe3b7a12da75479e52
Firgenius Kolose Hombore <firgeniusigeni7799@gmail.com>
2024-08-28 00:31:31 +0900

    ini program java

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.scanner;
+
+public class Luas Linggkaran
+{
+public static void main(string[]args){scanner input = new scanner (system.in);
+system.out.print("masukan jari-jari linggkaran:");
+double radius = input.nextDouble();
+double area= Math.PI*radius*radius;
+system.out.println("Luas Linggkaran adalah"+area);}}
